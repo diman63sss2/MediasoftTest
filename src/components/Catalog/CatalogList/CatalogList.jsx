cc3e905e28f3ffe26a9fe0a7f187a3574b9bb2e5
diman <diman63sss2@mail.ru>
2023-04-27 20:10:52 +0400

    sort, pagination.

--- src/components/Catalog/CatalogList/CatalogList.jsx
+++ src/components/Catalog/CatalogList/CatalogList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import cl from './CatalogList.module.css'
 import CatalogListItem from "../CatalogListItem/CatalogListItem";
-const CatalogList = ({products}) => {
-
+const CatalogList = ({products, filters}) => {
+    console.log('CatalogList')
+    console.log(filters.sort)
+    const indexOfLastPost = filters.page * filters.onPage;
+    const indexOfFirstPost = indexOfLastPost - filters.onPage;
+    const currentPosts = products.slice(indexOfFirstPost, indexOfLastPost);
     return (
         <div className={cl.list}>
-            {products.map(product =>
+            {currentPosts.map(product =>
                 <CatalogListItem product={product} key={product.id}/>
             )}
         </div>
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -2,11 +2,17 @@ import React, {useEffect, useState} from 'react';
 import {useFetching} from "../components/hooks/useFetching";
 import {getProducts} from "../http/catalogAPI";
 import CatalogList from "../components/Catalog/CatalogList/CatalogList";
+import CatalogFilters from "../components/Catalog/CatalogFilters/CatalogFilters";
+import CatalogPagination from "../components/Catalog/CatalogPagination/CatalogPagination";
 
 const Catalog = () => {
 
     const [products, setProducts] = useState([]);
-
+    const [filters, setFilters] = useState({
+        sort: "title",
+        page: 1,
+        onPage: 8
+    })
     const [fetchProducts, isProductsLoading] = useFetching(async () => {
         const response = await getProducts();
         console.log(response.data)
@@ -25,7 +31,20 @@ const Catalog = () => {
                     Загрузка
                 </div> :
                 <div className="container">
-                    <CatalogList products={products}/>
+                    <CatalogFilters filters={filters} setFilters={setFilters}/>
+                    <CatalogList products={products.sort(function(a,b){
+                        switch (filters.sort) {
+                            case 'title':
+                                return a.title.localeCompare(b.title);
+                            case 'price':
+                                return a.price - b.price;
+                            case 'rate':
+                                return a.rating.rate - b.rating.rate;
+                            case 'popular':
+                                return a.rating.count - b.rating.count;
+                        }
+                    })} filters={filters}/>
+                    <CatalogPagination elements={products.length} filters={filters} setFilters={setFilters}/>
                 </div>
             }
         </section>
